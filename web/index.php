c41cf502b5b4e489ffc272912bb37ea4cb54899a
Rickershot <32992138+Rickershot@users.noreply.github.com>
2017-10-21 20:33:57 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,13 +2,13 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=https://discordapp.com/oauth2/authorize?&client_id=343481708840222732&scope=bot">
+        <meta http-equiv="refresh" content="1;url=https://discordapp.com/oauth2/authorize?&client_id=371443948142592001&scope=bot">
         <script type="text/javascript">
-            window.location.href = "https://discordapp.com/oauth2/authorize?&client_id=343481708840222732&scope=bot"
+            window.location.href = "https://discordapp.com/oauth2/authorize?&client_id=371443948142592001&scope=bot"
         </script>
         <title>discord bot</title>
     </head>
     <body>
-        If you are not redirected automatically, follow the <a href='https://discordapp.com/oauth2/authorize?&client_id=343481708840222732&scope=bot'>link to the discord bot</a>
+        If you are not redirected automatically, follow the <a href='https://discordapp.com/oauth2/authorize?&client_id=371443948142592001&scope=bot'>link to the discord bot</a>
     </body>
 </html>
